bf2d8221ad980df06e1a5c04335c824272c5b919
Jardine <jardine.miller@gmail.com>
2022-09-22 10:37:54 +0100

    Added EFCore

--- Template.Infrastructure/DependencyInjection.cs
+++ Template.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -22,6 +23,7 @@ public static class DependencyInjection
     {
         services
             .AddAuth(configuration)
+            .AddDatabase(configuration)
             .AddScoped<IUserRepository, UserRepository>()
             .AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
@@ -35,8 +37,8 @@ public static class DependencyInjection
     {
         var jwtSettings = new JwtSettings();
         configuration.Bind(JwtSettings.SectionName, jwtSettings);
-
         services.AddSingleton(Options.Create(jwtSettings));
+
         services.AddSingleton<IJwtGenerator, JwtGenerator>();
 
         services
@@ -67,4 +69,67 @@ public static class DependencyInjection
 
         return services;
     }
+
+    private static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var databaseSettings = new DatabaseSettings();
+        configuration.Bind(
+            DatabaseSettings.SectionName,
+            databaseSettings
+        );
+        services.AddSingleton(Options.Create(databaseSettings));
+
+        services
+            .AddDbContext<ApplicationDbContext>(
+                options =>
+                {
+                    options.UseSqlServer(
+                        connectionString: databaseSettings.ConnectionString
+                    );
+                }
+            )
+            .ApplyMigrations();
+
+        return services;
+    }
+
+    private static IServiceCollection ApplyMigrations(
+        this IServiceCollection services
+    )
+    {
+        var dbContext = services
+            .BuildServiceProvider()
+            .GetRequiredService<ApplicationDbContext>();
+
+        dbContext?.Database.Migrate();
+
+        return services;
+    }
+
+    // private static IServiceCollection AddIdentity(
+    //     this IServiceCollection services,
+    //     IConfiguration configuration
+    // )
+    // {
+    //     services
+    //         .AddIdentityCore<User>()
+    //         .AddEntityFrameworkStores<ApplicationDbContext>();
+    //
+    //     services.Configure<IdentityOptions>(
+    //         options =>
+    //         {
+    //             options.Password.RequireDigit = false;
+    //             options.Password.RequiredLength = 6;
+    //             options.Password.RequireLowercase = false;
+    //             options.Password.RequireNonAlphanumeric = false;
+    //             options.Password.RequireUppercase = false;
+    //             options.Password.RequiredUniqueChars = 0;
+    //         }
+    //     );
+    //
+    //     return services;
+    // }
 }
